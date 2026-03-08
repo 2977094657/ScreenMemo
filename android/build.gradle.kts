1d8c249094536cc4116d248545a6e5d4e4df78f0
2977094657 <2977094657@qq.com>
2026-03-08 22:09:06 +0800

    build(android): fix Kotlin plugin mismatch for CI release

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,28 +4,6 @@ plugins {
     id("com.android.application") apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://storage.googleapis.com/download.flutter.io")
-        }
-        maven {
-            url = uri("http://download.flutter.io")
-            isAllowInsecureProtocol = true
-        }
-        // 可选国内镜像
-        maven { url = uri("https://repo.flutter-io.cn/download/flutter/io") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/maven-public") }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    }
-}
-
 allprojects {
     repositories {
         google()
